88d00ab98784860983ec52cb85ca9fd8bdd3336d
Yanis Wang <yanis.wang@gmail.com>
2013-03-23 18:04:50 +0800

    fix rule: attr-value-double-quotes

--- test/rules/attr-value-double-quotes.spec.js
+++ test/rules/attr-value-double-quotes.spec.js
@@ -10,16 +10,19 @@ var HTMLHint  = require("../../index").HTMLHint;
 describe('Rules: attr-value-double-quotes', function(){
 
     it('Attribute value closed by single quotes should result in an error', function(){
-        var code = '<a href=\'abc\'>';
+        var code = '<a href=\'abc\' title=abc>';
         var messages = HTMLHint.verify(code, {'attr-value-double-quotes': true});
-        expect(messages.length).to.be(1);
+        expect(messages.length).to.be(2);
         expect(messages[0].rule.id).to.be('attr-value-double-quotes');
         expect(messages[0].line).to.be(1);
         expect(messages[0].col).to.be(3);
+        expect(messages[1].rule.id).to.be('attr-value-double-quotes');
+        expect(messages[1].line).to.be(1);
+        expect(messages[1].col).to.be(14);
     });
 
     it('Attribute value no closed should not result in an error', function(){
-        var code = '<a href=abc>';
+        var code = '<input type="button" disabled>';
         var messages = HTMLHint.verify(code, {'attr-value-double-quotes': true});
         expect(messages.length).to.be(0);
     });
