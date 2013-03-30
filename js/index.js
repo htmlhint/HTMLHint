0d6309ee6a8b88fa7149076afaf6de34fa40a064
Yanis Wang <yanis.wang@gmail.com>
2013-03-30 20:39:53 +0800

    edit index

--- js/index.js
+++ js/index.js
@@ -1,72 +1,108 @@
 (function(){
     $.cookie.json = true;
+    
     //Default rules
     var ruleSets = {
-        'tagname-lowercase': true,
-        'attr-lowercase': true,
-        'attr-value-double-quotes': true,
-        'doctype-first': true,
-        'tag-pair': true,
-        'spec-char-escape': true
-    };
-    var ruleCSSLint = {
-        "display-property-grouping": true,
-        "known-properties": true
-    };
-    var ruleJSHint = {
-    };
+            'tagname-lowercase': true,
+            'attr-lowercase': true,
+            'attr-value-double-quotes': true,
+            'doctype-first': true,
+            'tag-pair': true,
+            'spec-char-escape': true
+        },
+        ruleCSSLint = {
+                "display-property-grouping": true,
+                "known-properties": true
+        },
+        ruleJSHint = {
+        };
+        
     var settings = {
-        editorTheme: 'merbivore'
-    };
-    var jEditorTheme = $('#editor-theme');
-    loadSettings();
-    jEditorTheme.val(settings.editorTheme);
-    jEditorTheme.change(function(){
-        settings.editorTheme = jEditorTheme.val();
-        editor.setTheme("ace/theme/"+settings.editorTheme);
-        saveSettings();
-    });
-    
-    loadRules();
-    //init editor
-    var editor = ace.edit("editor"),
-        upTimer,
+            editorTheme: 'merbivore'
+        };
+
+    var editor,
         arrHints = [];
-    editor.setShowPrintMargin(false);
-    editor.setTheme("ace/theme/"+settings.editorTheme);
-    editor.getSession().setMode("ace/mode/html");
-    editor.on('change', function(e){
-        clearTimeout(upTimer);
-        upTimer = setTimeout(updateHTMLHint, 500);
-    });
-    editor.commands.addCommand({
-        name: 'left',
-        bindKey: {win: 'Ctrl-Left',  mac: 'Command-Left'},
-        exec: showLastHint,
-        readOnly: true // false if this command should not apply in readOnly mode
-    });
-    editor.commands.addCommand({
-        name: 'up',
-        bindKey: {win: 'Ctrl-Up',  mac: 'Command-Up'},
-        exec: showLastHint,
-        readOnly: true // false if this command should not apply in readOnly mode
-    });
-    editor.commands.addCommand({
-        name: 'right',
-        bindKey: {win: 'Ctrl-Right',  mac: 'Command-Right'},
-        exec: showNextHint,
-        readOnly: true // false if this command should not apply in readOnly mode
-    });
-    editor.commands.addCommand({
-        name: 'down',
-        bindKey: {win: 'Ctrl-Down',  mac: 'Command-Down'},
-        exec: showNextHint,
-        readOnly: true // false if this command should not apply in readOnly mode
-    });
+        
     var jHintState = $('#hint-state'),
         jButtonArea = $('#button-area'),
         jShowLast = $('#show-last'),
         jShowNext = $('#show-next');
+        
+    loadSettings();
+    loadRules();
+    initEditor();
+    initOptions();
+    updateHTMLHint();
+
+    function loadSettings(){
+        var saveSettings = $.cookie('htmlhintSettings');
+        if(saveSettings){
+            settings = saveSettings;
+        }
+    }
+    
+    function saveSettings(){
+        $.cookie('htmlhintSettings', settings, { expires: 365 });
+    }
+    
+    function loadRules(){
+        var saveRuleSets = $.cookie('htmlhintRules');
+        if(saveRuleSets){
+            ruleSets = saveRuleSets;
+        }
+    }
+    
+    function saveRules(){
+        $.cookie('htmlhintRules', ruleSets, { expires: 365 });
+    }
+    
+    function initEditor(){
+        var jEditorTheme = $('#editor-theme');
+        
+        jEditorTheme.val(settings.editorTheme);
+        jEditorTheme.change(function(){
+            settings.editorTheme = jEditorTheme.val();
+            editor.setTheme("ace/theme/"+settings.editorTheme);
+            saveSettings();
+        });
+        var upTimer;
+        editor = ace.edit("editor")
+        editor.setShowPrintMargin(false);
+        editor.setTheme("ace/theme/"+settings.editorTheme);
+        editor.getSession().setMode("ace/mode/html");
+        editor.on('change', function(e){
+            clearTimeout(upTimer);
+            upTimer = setTimeout(updateHTMLHint, 500);
+        });
+        editor.commands.addCommand({
+            name: 'left',
+            bindKey: {win: 'Ctrl-Left',  mac: 'Command-Left'},
+            exec: showLastHint,
+            readOnly: true // false if this command should not apply in readOnly mode
+        });
+        editor.commands.addCommand({
+            name: 'up',
+            bindKey: {win: 'Ctrl-Up',  mac: 'Command-Up'},
+            exec: showLastHint,
+            readOnly: true // false if this command should not apply in readOnly mode
+        });
+        editor.commands.addCommand({
+            name: 'right',
+            bindKey: {win: 'Ctrl-Right',  mac: 'Command-Right'},
+            exec: showNextHint,
+            readOnly: true // false if this command should not apply in readOnly mode
+        });
+        editor.commands.addCommand({
+            name: 'down',
+            bindKey: {win: 'Ctrl-Down',  mac: 'Command-Down'},
+            exec: showNextHint,
+            readOnly: true // false if this command should not apply in readOnly mode
+        });
+        jShowLast.mousedown(showLastHint);
+        jShowNext.mousedown(showNextHint);
+    }
+
     function updateHTMLHint(){
         if(ruleSets['csslint'] === true){
             ruleSets['csslint'] = ruleCSSLint;
@@ -98,6 +134,7 @@
             jButtonArea.hide();
         }
     }
+    
     function showLastHint(){
         if(arrHints.length>0){
             var cursor = editor.selection.getCursor(),
@@ -117,6 +154,7 @@
         }
         return false;
     }
+    
     function showNextHint(){
         if(arrHints.length>0){
             var cursor = editor.selection.getCursor(),
@@ -136,60 +174,42 @@
         }
         return false;
     }
-    jShowLast.mousedown(showLastHint);
-    jShowNext.mousedown(showNextHint);
-    function loadSettings(){
-        var saveSettings = $.cookie('htmlhintSettings');
-        if(saveSettings){
-            settings = saveSettings;
-        }
-    }
-    function saveSettings(){
-        $.cookie('htmlhintSettings', settings, { expires: 365 });
-    }
-    function loadRules(){
-        var saveRuleSets = $.cookie('htmlhintRules');
-        if(saveRuleSets){
-            ruleSets = saveRuleSets;
-        }
-    }
-    function saveRules(){
-        $.cookie('htmlhintRules', ruleSets, { expires: 365 });
-    }
-    $('input[type=checkbox]').change(function(){
-        var jThis = $(this),
-            id = jThis.attr('id'),
-            ruleValue = jThis.prop('checked');
-        if(ruleValue === true){
-            var jValue = $('#'+id+'_value');
-            if(jValue.length > 0){
-                ruleValue = jValue.val();
+    
+    function initOptions(){
+        $('input[type=checkbox]').change(function(){
+            var jThis = $(this),
+                id = jThis.attr('id'),
+                ruleValue = jThis.prop('checked');
+            if(ruleValue === true){
+                var jValue = $('#'+id+'_value');
+                if(jValue.length > 0){
+                    ruleValue = jValue.val();
+                }
+                ruleSets[id] = ruleValue;
+                $('#'+id+'_valuearea').show();
             }
-            ruleSets[id] = ruleValue;
-            $('#'+id+'_valuearea').show();
-        }
-        else{
-            delete ruleSets[id];
-            $('#'+id+'_valuearea').hide();
-        }
-        saveRules();
-        updateHTMLHint();
-    });
-    $('select').change(function(){
-        var jThis = $(this),
-            id = jThis.attr('id').replace('_value', '');
-        ruleSets[id] = jThis.val();
-        saveRules();
-        updateHTMLHint();
-    });
-    for(var id in ruleSets){
-        var ruleValue = ruleSets[id],
-            jValue = $('#'+id+'_value');
-        $('#'+id).prop('checked', true);
-        if(jValue.length>0){
-            jValue.val(ruleValue);
-            $('#'+id+'_valuearea').show();
+            else{
+                delete ruleSets[id];
+                $('#'+id+'_valuearea').hide();
+            }
+            saveRules();
+            updateHTMLHint();
+        });
+        $('select').change(function(){
+            var jThis = $(this),
+                id = jThis.attr('id').replace('_value', '');
+            ruleSets[id] = jThis.val();
+            saveRules();
+            updateHTMLHint();
+        });
+        for(var id in ruleSets){
+            var ruleValue = ruleSets[id],
+                jValue = $('#'+id+'_value');
+            $('#'+id).prop('checked', true);
+            if(jValue.length>0){
+                jValue.val(ruleValue);
+                $('#'+id+'_valuearea').show();
+            }
         }
     }
-    updateHTMLHint();
 })();
