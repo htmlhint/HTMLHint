6bd3eae317601bb6b12ce2dbb41bde44250b2b5e
Yanis Wang <yanis.wang@gmail.com>
2013-04-02 15:26:00 +0800

    add rule: id-unique

---
+++ src/rules/id-unique.js
@@ -0,0 +1,36 @@
+/**
+ * Copyright (c) 2013, Yanis Wang <yanis.wang@gmail.com>
+ * MIT Licensed
+ */
+HTMLHint.addRule({
+    id: 'id-unique',
+    description: 'Id must be unique.',
+    init: function(parser, reporter){
+        var self = this;
+        var mapIdCount = {};
+        parser.addListener('tagstart', function(event){
+            var attrs = event.attrs,
+                attr,
+                id,
+                col = event.col + event.tagName.length + 1;
+            for(var i=0, l=attrs.length;i<l;i++){
+                attr = attrs[i];
+                if(attr.name.toLowerCase() === 'id'){
+                    id = attr.value;
+                    if(id){
+                        if(mapIdCount[id] === undefined){
+                            mapIdCount[id] = 1;
+                        }
+                        else{
+                            mapIdCount[id] ++;
+                        }
+                        if(mapIdCount[id] > 1){
+                            reporter.warn('Id redefinition of [ '+id+' ].', event.line, col + attr.index, self, attr.raw);
+                        }
+                    }
+                    break;
+                }
+            }
+        });
+    }
+});
---
+++ test/rules/id-unique.spec.js
@@ -0,0 +1,28 @@
+/**
+ * Copyright (c) 2013, Yanis Wang <yanis.wang@gmail.com>
+ * MIT Licensed
+ */
+
+var expect  = require("expect.js");
+
+var HTMLHint  = require("../../index").HTMLHint;
+
+describe('Rules: id-unique', function(){
+
+    it('Id redefine should result in an error', function(){
+        var code = '<div id="test"></div><div id="test"></div>';
+        var messages = HTMLHint.verify(code, {'id-unique': true});
+        expect(messages.length).to.be(1);
+        expect(messages[0].rule.id).to.be('id-unique');
+        expect(messages[0].line).to.be(1);
+        expect(messages[0].col).to.be(26);
+        expect(messages[0].type).to.be('warning');
+    });
+
+    it('Id no redefine should not result in an error', function(){
+        var code = '<div id="test1"></div><div id="test2"></div>';
+        var messages = HTMLHint.verify(code, {'id-unique': true});
+        expect(messages.length).to.be(0);
+    });
+
+});
