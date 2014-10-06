52c9ab1bbb0cb0e7a02ab921a76c2440d948a6b8
Waldir Pimenta <waldyrious@gmail.com>
2014-10-06 02:31:24 +0100

    grammar fix

--- src/rules/style-disabled.js
+++ src/rules/style-disabled.js
@@ -4,12 +4,12 @@
  */
 HTMLHint.addRule({
     id: 'style-disabled',
-    description: 'Style tag can not be use.',
+    description: 'Style tag can not be used.',
     init: function(parser, reporter){
         var self = this;
         parser.addListener('tagstart', function(event){
             if(event.tagName.toLowerCase() === 'style'){
-                reporter.warn('Style tag can not be use.', event.line, event.col, self, event.raw);
+                reporter.warn('Style tag can not be used.', event.line, event.col, self, event.raw);
             }
         });
     }
