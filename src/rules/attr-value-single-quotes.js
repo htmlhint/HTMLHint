37fbb81580d38ad962a2d2c900eaf9ddda1cb6f4
Donald Disha <dondished@gmail.com>
2015-08-18 11:16:52 -0400

    fixed trailing whitespace

--- src/rules/attr-value-single-quotes.js
+++ src/rules/attr-value-single-quotes.js
@@ -13,8 +13,7 @@ HTMLHint.addRule({
                 col = event.col + event.tagName.length + 1;
             for(var i=0, l=attrs.length;i<l;i++){
                 attr = attrs[i];
-                if((attr.value !== '' && attr.quote === '"') || 
-                    (attr.value === '' && attr.quote === '"')){ 
+                if((attr.value !== '' && attr.quote === '"') || (attr.value === '' && attr.quote === '"')){
                     reporter.error('The value of attribute [ '+attr.name+' ] must be in single quotes.', event.line, col + attr.index, self, attr.raw);
                 }
             }
