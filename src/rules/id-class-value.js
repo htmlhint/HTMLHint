6cd9de2f579ed46aa5711db5fb1eae1369d83934
molant <amolleda@gmail.com>
2016-04-15 13:40:54 -0700

    Adding support for custom RegExp in id-class-value rule Fixes #122

--- src/rules/id-class-value.js
+++ src/rules/id-class-value.js
@@ -5,7 +5,7 @@
 HTMLHint.addRule({
     id: 'id-class-value',
     description: 'The id and class attribute values must meet the specified rules.',
-    init: function(parser, reporter, options){
+    init: function (parser, reporter, options) {
         var self = this;
         var arrRules = {
             'underline': {
@@ -21,31 +21,38 @@ HTMLHint.addRule({
                 'message': 'The id and class attribute values must meet the camelCase style.'
             }
         }, rule;
-        if(typeof options === 'string'){
+        if (typeof options === 'string') {
             rule = arrRules[options];
+            //options is a regexp
+            if (!rule) {
+                rule = {
+                    'regId': new RegExp(options),
+                    'message': 'The id and class attribute values do not meet your regex'
+                };
+            }
         }
-        else{
+        else {
             rule = options;
         }
-        if(rule && rule.regId){
+        if (rule && rule.regId) {
             var regId = rule.regId,
                 message = rule.message;
-            parser.addListener('tagstart', function(event){
+            parser.addListener('tagstart', function (event) {
                 var attrs = event.attrs,
                     attr,
                     col = event.col + event.tagName.length + 1;
-                for(var i=0, l1=attrs.length;i<l1;i++){
+                for (var i = 0, l1 = attrs.length; i < l1; i++) {
                     attr = attrs[i];
-                    if(attr.name.toLowerCase() === 'id'){
-                        if(regId.test(attr.value) === false){
+                    if (attr.name.toLowerCase() === 'id') {
+                        if (regId.test(attr.value) === false) {
                             reporter.warn(message, event.line, col + attr.index, self, attr.raw);
                         }
                     }
-                    if(attr.name.toLowerCase() === 'class'){
+                    if (attr.name.toLowerCase() === 'class') {
                         var arrClass = attr.value.split(/\s+/g), classValue;
-                        for(var j=0, l2=arrClass.length;j<l2;j++){
+                        for (var j = 0, l2 = arrClass.length; j < l2; j++) {
                             classValue = arrClass[j];
-                            if(classValue && regId.test(classValue) === false){
+                            if (classValue && regId.test(classValue) === false) {
                                 reporter.warn(message, event.line, col + attr.index, self, classValue);
                             }
                         }
