fb9a26d1d2e2a72f0766c9b04afb11d8108f98b8
Ricardo Banffy <rbanffy@gmail.com>
2016-05-25 20:15:10 -0700

    Add test for &

--- test/rules/spec-char-escape.spec.js
+++ test/rules/spec-char-escape.spec.js
@@ -24,6 +24,18 @@ describe('Rules: '+ruldId, function(){
         expect(messages[2].col).to.be(4);
     });
 
+    it('Special characters: & should result in an error', function(){
+        var code = '<p>Steinway & Sons Q&A</p>';
+        var messages = HTMLHint.verify(code, ruleOptions);
+        expect(messages.length).to.be(2);
+        expect(messages[0].rule.id).to.be(ruldId);
+        expect(messages[0].line).to.be(1);
+        expect(messages[0].col).to.be(13);
+        expect(messages[1].rule.id).to.be(ruldId);
+        expect(messages[1].line).to.be(1);
+        expect(messages[1].col).to.be(21);
+    });
+
     it('Normal text should not result in an error', function(){
         var code = '<p>abc</p>';
         var messages = HTMLHint.verify(code, ruleOptions);
