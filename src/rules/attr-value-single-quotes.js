2e2d4b4e0dab64ff8785233e6a6aa1713c35102f
justin.fuller2 <justin.fuler2@fisglobal.com>
2016-06-27 11:00:10 -0400

    Add rule for single quotes instead of double quotes

---
+++ src/rules/attr-value-single-quotes.js
@@ -0,0 +1,19 @@
+HTMLHint.addRule({
+    id: 'attr-value-single-quotes',
+    description: 'Attribute values must be in single quotes.',
+    init: function(parser, reporter){
+        var self = this;
+        parser.addListener('tagstart', function(event){
+            var attrs = event.attrs,
+                attr,
+                col = event.col + event.tagName.length + 1;
+            for(var i=0, l=attrs.length;i<l;i++){
+                attr = attrs[i];
+                if((attr.value !== '' && attr.quote !== "'") ||
+                    (attr.value === '' && attr.quote === '"')){
+                    reporter.error('The value of attribute [ '+attr.name+' ] must be in single quotes.', event.line, col + attr.index, self, attr.raw);
+                }
+            }
+        });
+    }
+});
