80f65961a7bfb6216e08393d507ab200fcd069c0
a.obitskyi <a.obitskyi@astoundcommerce.com>
2016-12-19 20:02:53 +0200

    fix missing commas

--- test/rules/tags-check.spec.js
+++ test/rules/tags-check.spec.js
@@ -19,39 +19,39 @@ ruleOptions[ruldId] = {
 describe('Rules: ' + ruldId, function(){
 
     it('Tag <a> should have requered attrs [title, href]', function(){
-        var code = '<a>blabla</a>'
+        var code = '<a>blabla</a>';
         var messages = HTMLHint.verify(code, ruleOptions);
         expect(messages.length).to.be(2);
         expect(messages[0].rule.id).to.be(ruldId);
         expect(messages[1].rule.id).to.be(ruldId);
     });
     it('Tag <a> should not be selfclosing', function(){
-        var code = '<a href="bbb" title="aaa"/>'
+        var code = '<a href="bbb" title="aaa"/>';
         var messages = HTMLHint.verify(code, ruleOptions);
         expect(messages.length).to.be(1);
         expect(messages[0].rule.id).to.be(ruldId);
     });
     it('Tag <img> should be selfclosing', function(){
-        var code = '<img src="bbb" title="aaa" alt="asd"></img>'
+        var code = '<img src="bbb" title="aaa" alt="asd"></img>';
         var messages = HTMLHint.verify(code, ruleOptions);
         expect(messages.length).to.be(1);
         expect(messages[0].rule.id).to.be(ruldId);
     });
     it('Should check optional attributes', function(){
-        var code = '<script src="aaa" async="sad"  />'
+        var code = '<script src="aaa" async="sad" />';
         var messages = HTMLHint.verify(code, ruleOptions);
         expect(messages.length).to.be(1);
         expect(messages[0].rule.id).to.be(ruldId);
     });
     it('Should check redunant attributes', function(){
-        var code = '<main role="main" />'
+        var code = '<main role="main" />';
         var messages = HTMLHint.verify(code, ruleOptions);
         expect(messages.length).to.be(2);
         expect(messages[0].rule.id).to.be(ruldId);
         expect(messages[1].rule.id).to.be(ruldId);
     });
     it('Should be extendable trought config', function(){
-        var code = '<sometag></sometag>'
+        var code = '<sometag></sometag>';
         var messages = HTMLHint.verify(code, ruleOptions);
         expect(messages.length).to.be(1);
         expect(messages[0].rule.id).to.be(ruldId);
