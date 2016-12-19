6e70eff8376c71d0bf57fbc40197a395db8eccfa
a.obitskyi <a.obitskyi@astoundcommerce.com>
2016-12-19 20:10:48 +0200

    add polifil for old JS engines

--- src/rules/tags-check.js
+++ src/rules/tags-check.js
@@ -27,6 +27,31 @@ var tagsTypings = {
     }
 };
 
+var assign = function(target, firstSource) {
+    'use strict';
+    if (target === undefined || target === null) {
+        throw new TypeError('Cannot convert first argument to object');
+    }
+
+    var to = Object(target);
+    for (var i = 1; i < arguments.length; i++) {
+        var nextSource = arguments[i];
+        if (nextSource === undefined || nextSource === null) {
+            continue;
+        }
+
+        var keysArray = Object.keys(Object(nextSource));
+        for (var nextIndex = 0, len = keysArray.length; nextIndex < len; nextIndex++) {
+            var nextKey = keysArray[nextIndex];
+            var desc = Object.getOwnPropertyDescriptor(nextSource, nextKey);
+            if (desc !== undefined && desc.enumerable) {
+                to[nextKey] = nextSource[nextKey];
+            }
+        }
+    }
+    return to;
+};
+
 HTMLHint.addRule({
     id: 'tags-check',
     description: 'Checks html tags.',
@@ -34,7 +59,7 @@ HTMLHint.addRule({
         var self = this;
 
         if (typeof options !== 'boolean') {
-            Object.assign(tagsTypings, options);
+            assign(tagsTypings, options);
         }
 
         parser.addListener('tagstart', function (event) {
