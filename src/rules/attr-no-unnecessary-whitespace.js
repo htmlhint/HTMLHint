87d56bd6049276de950ab349adbd017203e3b827
pcfutures <me@jaketaylor.co>
2017-10-11 14:33:13 +0100

    remove console.log statement

--- src/rules/attr-no-unnecessary-whitespace.js
+++ src/rules/attr-no-unnecessary-whitespace.js
@@ -1,14 +1,13 @@
 HTMLHint.addRule({
     id: 'attr-no-unnecessary-whitespace',
     description: 'No spaces between attribute names and values.',
-    init: function(parser, reporter, options){
+    init: function (parser, reporter, options) {
         var self = this;
         var exceptions = Array.isArray(options) ? options : [];
-        parser.addListener('tagstart', function(event){
+        parser.addListener('tagstart', function (event) {
             var attrs = event.attrs,
                 col = event.col + event.tagName.length + 1;
-            for(var i = 0; i < attrs.length; i++){
-                console.log(attrs[i]);
+            for (var i = 0; i < attrs.length; i++) {
                 if (exceptions.indexOf(attrs[i].name) === -1 && /[^=](\s+=\s+|=\s+|\s+=)/g.test(attrs[i].raw.trim())) {
                     reporter.error(
                         'The attribute \'' + attrs[i].name + '\' must not have spaces between the name and value.',
