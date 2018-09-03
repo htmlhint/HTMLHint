5cf83482e692b1570c2cc52b9382ff1923a78783
Aaron K <aaronkahlhamer@gmail.com>
2018-09-03 10:01:26 -0500

    chore(htmlhint): adding support for a white list of elements that can be lower case. (#188)
    When enabling the rule instead of using a boolean, use an array with the attributes (camelCased) that you want to ignore
    ```json
    {
      "tagname-lowercase": ["clipPath"]
    }
    ```

--- src/rules/tagname-lowercase.js
+++ src/rules/tagname-lowercase.js
@@ -1,11 +1,12 @@
 HTMLHint.addRule({
     id: 'tagname-lowercase',
     description: 'All html element names must be in lowercase.',
-    init: function(parser, reporter){
+    init: function(parser, reporter, options){
         var self = this;
+        var exceptions = Array.isArray(options) ? options : [];
         parser.addListener('tagstart,tagend', function(event){
             var tagName = event.tagName;
-            if(tagName !== tagName.toLowerCase()){
+            if (exceptions.indexOf(tagName) === -1 && tagName !== tagName.toLowerCase()){
                 reporter.error('The html element name of [ '+tagName+' ] must be in lowercase.', event.line, event.col, self, event.raw);
             }
         });
