bfb7a75522a0089dd72868c55ff1e3d8fcb9b957
David Dias <thedaviddias@gmail.com>
2018-09-04 14:22:38 -0400

    feat(htmlhint): add script-disabled rule (#301)

---
+++ src/rules/script-disabled.js
@@ -0,0 +1,21 @@
+HTMLHint.addRule({
+  id: 'script-disabled',
+  description: 'The <script> tag cannot be used.',
+  init: function(parser, reporter) {
+    'use strict';
+
+    var self = this;
+
+    parser.addListener('tagstart', function(event) {
+      if (event.tagName.toLowerCase() === 'script') {
+        reporter.error(
+          'The <script> tag cannot be used.',
+          event.line,
+          event.col,
+          self,
+          event.raw
+        );
+      }
+    });
+  }
+});
