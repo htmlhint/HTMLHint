30f01d9382cfbb57c0c4d7ca9528d82b99077010
nmanupuri <39561219+nmanupuri@users.noreply.github.com>
2019-02-24 05:23:16 +0530

    html.js (#304)
    This is a new formatter which logs error messages in html format on console as well as it will generate a report file in html format
    
    **Fixes**: #
    
    🚨 Please review the [guidelines for contributing](CONTRIBUTING.md) and our [code of conduct](../CODE_OF_CONDUCT.md) to this repository. 🚨
    **Please complete these steps and check these boxes (by putting an x inside the brackets) before filing your PR:**
    
    - [x] Check the commit's or even all commits' message styles matches our requested structure.
    - [x] Check your code additions will fail neither code linting checks nor unit test.
    
    #### Short description of what this resolves:
    New formatter
    
    #### Proposed changes:
    New formatter
    
    ## -
    
    -
    
    👍 Thank you!

---
+++ bin/formatters/html.js
@@ -0,0 +1,26 @@
+var fs = require('fs');
+
+var htmlFormatter = function(formatter){
+    formatter.on('end', function(event){
+        var fileContent;
+        fileContent = '<html>';
+        fileContent = fileContent + '<head><title>HTML Hint Violation Report</title></head>';
+        fileContent = fileContent + '<body>';
+        fileContent = fileContent + '<center><h2>Violation Report</h2></center>';
+
+        fileContent = fileContent + '<table border = 1>';
+        fileContent = fileContent + '<tr><th>Number#</th><th>File Name</th><th>Line Number</th><th>Message</th></tr>';
+        var arrAllMessages = event.arrAllMessages;
+        arrAllMessages.forEach(function(fileInfo){
+            var arrMessages = fileInfo.messages;
+            arrMessages.forEach(function(message){
+                fileContent = fileContent + '<tr><td>'+(i+1)+'</td><td>'+fileInfo.file+'</td><td>'+message.line+'</td><td>'+message.message+'</td></tr>';
+            });
+        });
+        fileContent = fileContent.replace('</table></body></html>');
+        console.log(fileContent);
+        fs.writeFileSync('report.html', fileContent);
+    });
+};
+
+module.exports = htmlFormatter;
