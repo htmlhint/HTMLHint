e44a5e88de1fa787c83f18a13502bef9a0b5c1f1
Zhixing Zhang <me@neoto.xin>
2019-02-24 10:36:21 -0600

    fix: use lib/ as the output dir

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   mode: 'production',
   output: {
     filename: 'htmlhint.js',
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'lib'),
     library: 'HTMLHint',
     libraryTarget: 'umd',
     // See https://github.com/webpack/webpack/issues/6784
