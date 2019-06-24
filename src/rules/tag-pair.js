c51e59a7a4147447e566987b59aeb87d6ba26f64
Shoji Matsumoto <shom@revolver.co.jp>
2019-06-24 20:07:52 +0900

    tag-pair allows optional close tags

--- src/rules/tag-pair.js
+++ src/rules/tag-pair.js
@@ -3,18 +3,65 @@ export default {
   description: 'Tag must be paired.',
   init: function(parser, reporter) {
     var self = this;
+    
+    function makeComplexMap(arr){
+      return arr.reduce(function(p,c){
+        c[0].split(',').forEach(function(v){
+          p[v] = parser.makeMap(c[1]);
+        });
+        return p;
+      }, {});
+    }
+    function inComplexMap(complexMap, left, right){
+      return !!(left && right && complexMap[left] && complexMap[left][right]);
+    }
+    
     var stack = [],
+      
+      // https://html.spec.whatwg.org/multipage/syntax.html#optional-tags
+      prevClosingTags = makeComplexMap([
+        // [targettags, prevtags]
+        ['li', 'li'],
+        ['dt,dd', 'dt,dd'],
+        ['option','option'],
+        ['optgroup','optgroup,option'],
+        ['rt,rp', 'rt,rp'],
+        ['tr,td,th', 'tr,td,th'],
+        ['address,article,aside,blockquote,details,'+
+           'div,dl,fieldset,figcaption,figure,footer'+
+           'form,h1,h2,h3,h4,h5,h6,header,hgroup,'+
+           'hr,main,menu,nav,ol,p,pre,section,table,ul',
+          'p'],
+      ]),
+      lastChildClosingTags = makeComplexMap([
+        // targettags, lastchildtags
+        ['ul,ol', 'li'],
+        ['dl', 'dt,dd'],
+        ['optgroup', 'option'],
+        ['select', 'optgroup,option'],
+        ['ruby', 'rt,rp'],
+        ['table,tbody,thead,tfoot','tr,td,th,thead,tbody,tfoot'],
+      ]),
+    
       mapEmptyTags = parser.makeMap(
         'area,base,basefont,br,col,frame,hr,img,input,isindex,link,meta,param,embed,track,command,source,keygen,wbr'
       ); //HTML 4.01 + HTML 5
     parser.addListener('tagstart', function(event) {
       var tagName = event.tagName.toLowerCase();
       if (mapEmptyTags[tagName] === undefined && !event.close) {
-        stack.push({
-          tagName: tagName,
-          line: event.line,
-          raw: event.raw
-        });
+
+        var lastTagName = stack.length && stack[stack.length-1].tagName;
+        if (inComplexMap(prevClosingTags, tagName, lastTagName)) {
+          // auto close sequenced tags
+        } else {
+          
+          stack.push({
+              tagName: tagName,
+              line: event.line,
+              raw: event.raw
+            });
+          
+        }
       }
     });
     parser.addListener('tagend', function(event) {
@@ -26,6 +73,17 @@ export default {
         }
       }
       if (pos >= 0) {
+
+        // auto close last child
+        while(stack.length){
+          var lastTagName = stack[stack.length-1].tagName;
+          if (inComplexMap(lastChildClosingTags, tagName, lastTagName)) {
+            stack.pop();
+          } else {
+            break;
+          }
+        }
+
         var arrTags = [];
         for (var i = stack.length - 1; i > pos; i--) {
           arrTags.push('</' + stack[i].tagName + '>');
