68aeac52ff09f11b156f3f8f603c65ee7f47c373
David Dias <thedaviddias@gmail.com>
2020-05-16 10:21:49 -0400

    fix require hint (#379)

--- test/core.spec.js
+++ test/core.spec.js
@@ -1,6 +1,6 @@
 const expect = require('expect.js');
 
-const HTMLHint = require('../dist/htmlhint.js').hint;
+const HTMLHint = require('../dist/htmlhint.js').HTMLHint;
 
 describe('Core', function() {
   it('Set false to rule no effected should result in an error', function() {
