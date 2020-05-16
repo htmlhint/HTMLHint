518a1a23f7dc93dc53ad15a975024a59968e5e41
David Dias <thedaviddias@gmail.com>
2020-05-16 16:08:34 -0400

    feat: Add tags checking rule - allows specify rules for any tag and validate that (#384)
    * adding tags check rule
    
    * fix missing commas
    
    * add polifil for old JS engines
    
    * add polifil for old JS engines
    
    * fix missing commas
    
    * fix indexOf
    
    * incrace code covarage
    
    * incrace code covarage
    
    * review fix
    
    * fix formating
    
    * fixing issues
    
    Co-authored-by: a.obitskyi <a.obitskyi@astoundcommerce.com>

--- test/rules/tags-check.spec.js
+++ test/rules/tags-check.spec.js
@@ -1,62 +1,63 @@
-const expect = require('expect.js')
 
-const HTMLHint = require('../../dist/htmlhint.js').HTMLHint
+const expect  = require("expect.js");
 
-const ruldId = 'tags-check'
-const ruleOptions = {}
+const HTMLHint = require('../../dist/htmlhint.js').HTMLHint;
+
+const ruldId = 'tags-check',
+  ruleOptions = {};
 
 ruleOptions[ruldId] = {
   sometag: {
     selfclosing: true,
-    attrsRequired: [['attrname', 'attrvalue']],
-  },
-}
+    attrsRequired: [['attrname', 'attrvalue']]
+  }
+};
 
-describe('Rules: ' + ruldId, function () {
-  it('Tag <a> should have requered attrs [title, href]', function () {
-    var code = '<a>blabla</a>'
-    var messages = HTMLHint.verify(code, ruleOptions)
-    expect(messages.length).to.be(2)
-    expect(messages[0].rule.id).to.be(ruldId)
-    expect(messages[1].rule.id).to.be(ruldId)
-  })
-  it('Tag <a> should not be selfclosing', function () {
-    var code = '<a href="bbb" title="aaa"/>'
-    var messages = HTMLHint.verify(code, ruleOptions)
-    expect(messages.length).to.be(1)
-    expect(messages[0].rule.id).to.be(ruldId)
-  })
-  it('Tag <img> should be selfclosing', function () {
-    var code = '<img src="bbb" title="aaa" alt="asd"></img>'
-    var messages = HTMLHint.verify(code, ruleOptions)
-    expect(messages.length).to.be(1)
-    expect(messages[0].rule.id).to.be(ruldId)
-  })
-  it('Should check optional attributes', function () {
-    var code = '<script src="aaa" async="sad" />'
-    var messages = HTMLHint.verify(code, ruleOptions)
-    expect(messages.length).to.be(1)
-    expect(messages[0].rule.id).to.be(ruldId)
-  })
-  it('Should check redunant attributes', function () {
-    var code = '<main role="main" />'
-    var messages = HTMLHint.verify(code, ruleOptions)
-    expect(messages.length).to.be(2)
-    expect(messages[0].rule.id).to.be(ruldId)
-    expect(messages[1].rule.id).to.be(ruldId)
-  })
-  it('Should be extendable trought config', function () {
-    var code = '<sometag></sometag>'
-    var messages = HTMLHint.verify(code, ruleOptions)
-    expect(messages.length).to.be(2)
-    expect(messages[0].rule.id).to.be(ruldId)
-  })
-  it('Should check required attributes with specifyed values', function () {
-    var code = '<sometag attrname="attrvalue" />'
-    var messages = HTMLHint.verify(code, ruleOptions)
-    expect(messages.length).to.be(0)
-    code = '<sometag attrname="wrong_value" />'
-    messages = HTMLHint.verify(code, ruleOptions)
-    expect(messages.length).to.be(1)
-  })
-})
+describe('Rules: ' + ruldId, function(){
+  it('Tag <a> should have requered attrs [title, href]', function(){
+    var code = '<a>blabla</a>';
+    var messages = HTMLHint.verify(code, ruleOptions);
+    expect(messages.length).to.be(2);
+    expect(messages[0].rule.id).to.be(ruldId);
+    expect(messages[1].rule.id).to.be(ruldId);
+  });
+  it('Tag <a> should not be selfclosing', function(){
+    var code = '<a href="bbb" title="aaa"/>';
+    var messages = HTMLHint.verify(code, ruleOptions);
+    expect(messages.length).to.be(1);
+    expect(messages[0].rule.id).to.be(ruldId);
+  });
+  it('Tag <img> should be selfclosing', function(){
+    var code = '<img src="bbb" title="aaa" alt="asd"></img>';
+    var messages = HTMLHint.verify(code, ruleOptions);
+    expect(messages.length).to.be(1);
+    expect(messages[0].rule.id).to.be(ruldId);
+  });
+  it('Should check optional attributes', function(){
+    var code = '<script src="aaa" async="sad" />';
+    var messages = HTMLHint.verify(code, ruleOptions);
+    expect(messages.length).to.be(1);
+    expect(messages[0].rule.id).to.be(ruldId);
+  });
+  it('Should check redunant attributes', function(){
+    var code = '<main role="main" />';
+    var messages = HTMLHint.verify(code, ruleOptions);
+    expect(messages.length).to.be(2);
+    expect(messages[0].rule.id).to.be(ruldId);
+    expect(messages[1].rule.id).to.be(ruldId);
+  });
+  it('Should be extendable trought config', function(){
+    var code = '<sometag></sometag>';
+    var messages = HTMLHint.verify(code, ruleOptions);
+    expect(messages.length).to.be(2);
+    expect(messages[0].rule.id).to.be(ruldId);
+  });
+  it('Should check required attributes with specifyed values', function(){
+    var code = '<sometag attrname="attrvalue" />';
+    var messages = HTMLHint.verify(code, ruleOptions);
+    expect(messages.length).to.be(0);
+    code = '<sometag attrname="wrong_value" />';
+    messages = HTMLHint.verify(code, ruleOptions);
+    expect(messages.length).to.be(1);
+  });
+});
