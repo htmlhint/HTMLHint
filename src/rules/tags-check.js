efff59648b8ba50606de651edc938e483944c980
David Dias <thedaviddias@gmail.com>
2020-05-16 15:27:30 -0400

    fixing issues

--- src/rules/tags-check.js
+++ src/rules/tags-check.js
@@ -27,7 +27,7 @@ var tagsTypings = {
     }
 };
 
-function assign(target) {
+var assign = function(target) {
     var _source;
 
     for (var i = 1; i < arguments.length; i++) {
@@ -39,80 +39,80 @@ function assign(target) {
     return target;
 }
 
-HTMLHint.addRule({
+export default {
     id: 'tags-check',
     description: 'Checks html tags.',
     init: function (parser, reporter, options) {
-        var self = this;
+      var self = this;
 
-        if (typeof options !== 'boolean') {
-            assign(tagsTypings, options);
-        }
+      if (typeof options !== 'boolean') {
+          assign(tagsTypings, options);
+      }
 
-        parser.addListener('tagstart', function (event) {
-            var attrs = event.attrs;
-            var col = event.col + event.tagName.length + 1;
+      parser.addListener('tagstart', function (event) {
+          var attrs = event.attrs;
+          var col = event.col + event.tagName.length + 1;
 
-            var tagName = event.tagName.toLowerCase();
+          var tagName = event.tagName.toLowerCase();
 
-            if (tagsTypings[tagName]) {
-                var currentTagType = tagsTypings[tagName];
+          if (tagsTypings[tagName]) {
+              var currentTagType = tagsTypings[tagName];
 
-                if (currentTagType.selfclosing === true && !event.close) {
-                    reporter.warn('The <' + tagName + '> tag must be selfclosing.', event.line, event.col, self, event.raw);
-                } else if (currentTagType.selfclosing === false && event.close) {
-                    reporter.warn('The <' + tagName +'> tag must not be selfclosing.', event.line, event.col, self, event.raw);
-                }
+              if (currentTagType.selfclosing === true && !event.close) {
+                  reporter.warn('The <' + tagName + '> tag must be selfclosing.', event.line, event.col, self, event.raw);
+              } else if (currentTagType.selfclosing === false && event.close) {
+                  reporter.warn('The <' + tagName +'> tag must not be selfclosing.', event.line, event.col, self, event.raw);
+              }
 
-                if (currentTagType.attrsRequired) {
-                    currentTagType.attrsRequired.forEach(function (id) {
-                        if (Array.isArray(id)) {
-                            var copyOfId = id.map(function (a) { return a;});
-                            var realID = copyOfId.shift();
-                            var values = copyOfId;
+              if (currentTagType.attrsRequired) {
+                  currentTagType.attrsRequired.forEach(function (id) {
+                      if (Array.isArray(id)) {
+                          var copyOfId = id.map(function (a) { return a;});
+                          var realID = copyOfId.shift();
+                          var values = copyOfId;
 
-                            if (attrs.some(function (attr) {return attr.name === realID;})) {
-                                attrs.forEach(function (attr) {
-                                    if (attr.name === realID && values.indexOf(attr.value) === -1) {
-                                        reporter.error('The <' + tagName +'> tag must have attr \'' + realID + '\' with one value of \'' + values.join('\' or \'') + '\'.', event.line, col, self, event.raw);
-                                    }
-                                });
-                            } else {
-                                reporter.error('The <' + tagName + '> tag must have attr \'' + realID + '\'.', event.line, col, self, event.raw);
-                            }
-                        } else if (!attrs.some(function (attr) {return id.split('|').indexOf(attr.name) !== -1;})) {
-                            reporter.error('The <' + tagName + '> tag must have attr \'' + id + '\'.', event.line, col, self, event.raw);
-                        }
-                    });
-                }
-                if (currentTagType.attrsOptional) {
-                    currentTagType.attrsOptional.forEach(function (id) {
-                        if (Array.isArray(id)) {
-                            var copyOfId = id.map(function (a) { return a;});
-                            var realID = copyOfId.shift();
-                            var values = copyOfId;
+                          if (attrs.some(function (attr) {return attr.name === realID;})) {
+                              attrs.forEach(function (attr) {
+                                  if (attr.name === realID && values.indexOf(attr.value) === -1) {
+                                      reporter.error('The <' + tagName +'> tag must have attr \'' + realID + '\' with one value of \'' + values.join('\' or \'') + '\'.', event.line, col, self, event.raw);
+                                  }
+                              });
+                          } else {
+                              reporter.error('The <' + tagName + '> tag must have attr \'' + realID + '\'.', event.line, col, self, event.raw);
+                          }
+                      } else if (!attrs.some(function (attr) {return id.split('|').indexOf(attr.name) !== -1;})) {
+                          reporter.error('The <' + tagName + '> tag must have attr \'' + id + '\'.', event.line, col, self, event.raw);
+                      }
+                  });
+              }
+              if (currentTagType.attrsOptional) {
+                  currentTagType.attrsOptional.forEach(function (id) {
+                      if (Array.isArray(id)) {
+                          var copyOfId = id.map(function (a) { return a;});
+                          var realID = copyOfId.shift();
+                          var values = copyOfId;
 
-                            if (attrs.some(function (attr) {return attr.name === realID;})) {
-                                attrs.forEach(function (attr) {
-                                    if (attr.name === realID && values.indexOf(attr.value) === -1) {
-                                        reporter.error('The <' + tagName + '> tag must have optional attr \'' + realID +
-                                            '\' with one value of \'' + values.join('\' or \'') + '\'.', event.line, col, self, event.raw);
-                                    }
-                                });
-                            }
-                        }
-                    });
-                }
+                          if (attrs.some(function (attr) {return attr.name === realID;})) {
+                              attrs.forEach(function (attr) {
+                                  if (attr.name === realID && values.indexOf(attr.value) === -1) {
+                                      reporter.error('The <' + tagName + '> tag must have optional attr \'' + realID +
+                                          '\' with one value of \'' + values.join('\' or \'') + '\'.', event.line, col, self, event.raw);
+                                  }
+                              });
+                          }
+                      }
+                  });
+              }
 
-                if (currentTagType.redundantAttrs) {
-                    currentTagType.redundantAttrs.forEach(function (attrName) {
-                        if (attrs.some(function (attr) { return attr.name === attrName;})) {
-                            reporter.error('The attr \'' + attrName + '\' is redundant for <' + tagName + '> and should be ommited.', event.line, col, self, event.raw);
-                        }
-                    });
-                }
+              if (currentTagType.redundantAttrs) {
+                  currentTagType.redundantAttrs.forEach(function (attrName) {
+                      if (attrs.some(function (attr) { return attr.name === attrName;})) {
+                          reporter.error('The attr \'' + attrName + '\' is redundant for <' + tagName + '> and should be ommited.', event.line, col, self, event.raw);
+                      }
+                  });
+              }
 
-            }
-        });
+          }
+      });
     }
-});
+};
--- test/rules/tags-check.spec.js
+++ test/rules/tags-check.spec.js
@@ -1,70 +1,63 @@
-/**
- * Copyright (c) 2015, Yanis Wang <yanis.wang@gmail.com>
- * MIT Licensed
- */
 
-var expect  = require("expect.js");
+const expect  = require("expect.js");
 
-var HTMLHint  = require("../../index").HTMLHint;
+const HTMLHint = require('../../dist/htmlhint.js').HTMLHint;
 
-var ruldId = 'tags-check',
-    ruleOptions = {};
+const ruldId = 'tags-check',
+  ruleOptions = {};
 
 ruleOptions[ruldId] = {
-    sometag: {
-        selfclosing: true,
-        attrsRequired: [['attrname', 'attrvalue']]
-    }
+  sometag: {
+    selfclosing: true,
+    attrsRequired: [['attrname', 'attrvalue']]
+  }
 };
 
 describe('Rules: ' + ruldId, function(){
-
-    it('Tag <a> should have requered attrs [title, href]', function(){
-        var code = '<a>blabla</a>';
-        var messages = HTMLHint.verify(code, ruleOptions);
-        expect(messages.length).to.be(2);
-        expect(messages[0].rule.id).to.be(ruldId);
-        expect(messages[1].rule.id).to.be(ruldId);
-    });
-    it('Tag <a> should not be selfclosing', function(){
-        var code = '<a href="bbb" title="aaa"/>';
-        var messages = HTMLHint.verify(code, ruleOptions);
-        expect(messages.length).to.be(1);
-        expect(messages[0].rule.id).to.be(ruldId);
-    });
-    it('Tag <img> should be selfclosing', function(){
-        var code = '<img src="bbb" title="aaa" alt="asd"></img>';
-        var messages = HTMLHint.verify(code, ruleOptions);
-        expect(messages.length).to.be(1);
-        expect(messages[0].rule.id).to.be(ruldId);
-    });
-    it('Should check optional attributes', function(){
-        var code = '<script src="aaa" async="sad" />';
-        var messages = HTMLHint.verify(code, ruleOptions);
-        expect(messages.length).to.be(1);
-        expect(messages[0].rule.id).to.be(ruldId);
-    });
-    it('Should check redunant attributes', function(){
-        var code = '<main role="main" />';
-        var messages = HTMLHint.verify(code, ruleOptions);
-        expect(messages.length).to.be(2);
-        expect(messages[0].rule.id).to.be(ruldId);
-        expect(messages[1].rule.id).to.be(ruldId);
-    });
-    it('Should be extendable trought config', function(){
-        var code = '<sometag></sometag>';
-        var messages = HTMLHint.verify(code, ruleOptions);
-        expect(messages.length).to.be(2);
-        expect(messages[0].rule.id).to.be(ruldId);
-        
-    });
-    it('Should check required attributes with specifyed values', function(){
-        var code = '<sometag attrname="attrvalue" />';
-        var messages = HTMLHint.verify(code, ruleOptions);
-        expect(messages.length).to.be(0);
-        code = '<sometag attrname="wrong_value" />';
-        messages = HTMLHint.verify(code, ruleOptions);
-        expect(messages.length).to.be(1);
-    });
-
+  it('Tag <a> should have requered attrs [title, href]', function(){
+    var code = '<a>blabla</a>';
+    var messages = HTMLHint.verify(code, ruleOptions);
+    expect(messages.length).to.be(2);
+    expect(messages[0].rule.id).to.be(ruldId);
+    expect(messages[1].rule.id).to.be(ruldId);
+  });
+  it('Tag <a> should not be selfclosing', function(){
+    var code = '<a href="bbb" title="aaa"/>';
+    var messages = HTMLHint.verify(code, ruleOptions);
+    expect(messages.length).to.be(1);
+    expect(messages[0].rule.id).to.be(ruldId);
+  });
+  it('Tag <img> should be selfclosing', function(){
+    var code = '<img src="bbb" title="aaa" alt="asd"></img>';
+    var messages = HTMLHint.verify(code, ruleOptions);
+    expect(messages.length).to.be(1);
+    expect(messages[0].rule.id).to.be(ruldId);
+  });
+  it('Should check optional attributes', function(){
+    var code = '<script src="aaa" async="sad" />';
+    var messages = HTMLHint.verify(code, ruleOptions);
+    expect(messages.length).to.be(1);
+    expect(messages[0].rule.id).to.be(ruldId);
+  });
+  it('Should check redunant attributes', function(){
+    var code = '<main role="main" />';
+    var messages = HTMLHint.verify(code, ruleOptions);
+    expect(messages.length).to.be(2);
+    expect(messages[0].rule.id).to.be(ruldId);
+    expect(messages[1].rule.id).to.be(ruldId);
+  });
+  it('Should be extendable trought config', function(){
+    var code = '<sometag></sometag>';
+    var messages = HTMLHint.verify(code, ruleOptions);
+    expect(messages.length).to.be(2);
+    expect(messages[0].rule.id).to.be(ruldId);
+  });
+  it('Should check required attributes with specifyed values', function(){
+    var code = '<sometag attrname="attrvalue" />';
+    var messages = HTMLHint.verify(code, ruleOptions);
+    expect(messages.length).to.be(0);
+    code = '<sometag attrname="wrong_value" />';
+    messages = HTMLHint.verify(code, ruleOptions);
+    expect(messages.length).to.be(1);
+  });
 });
