72e818b8e49a2b433ab4ec22a863bc98e402378c
David Dias <thedaviddias@gmail.com>
2020-05-16 21:18:44 -0400

    fix: prettify more files

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,18 +1,18 @@
 /* eslint-env node */
-const { types, scopes, allowCustomScopes } = require('./commitizen.config');
+const { types, scopes, allowCustomScopes } = require('./commitizen.config')
 
-const validTypes = types.map(type => type.value);
-const validScopes = scopes.map(scope => scope.name);
-const scopeValidationLevel = allowCustomScopes ? 1 : 2;
+const validTypes = types.map((type) => type.value)
+const validScopes = scopes.map((scope) => scope.name)
+const scopeValidationLevel = allowCustomScopes ? 1 : 2
 
 module.exports = {
-    extends: ['@commitlint/config-conventional'],
-    parserPreset: './parser-preset',
+  extends: ['@commitlint/config-conventional'],
+  parserPreset: './parser-preset',
 
-    // Add your own rules. See http://marionebl.github.io/commitlint
-    rules: {
-        // Apply valid scopes and types
-        'scope-enum': [scopeValidationLevel, 'always', validScopes],
-        'type-enum': [2, 'always', validTypes]
-    }
-};
+  // Add your own rules. See http://marionebl.github.io/commitlint
+  rules: {
+    // Apply valid scopes and types
+    'scope-enum': [scopeValidationLevel, 'always', validScopes],
+    'type-enum': [2, 'always', validTypes],
+  },
+}
