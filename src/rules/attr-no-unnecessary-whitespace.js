de121351513d25d5c6ea9b65282540dc20db6ebc
Christopher Quadflieg <chrissi92@hotmail.de>
2020-05-19 08:32:05 +0200

    chore: use const

--- src/rules/attr-no-unnecessary-whitespace.js
+++ src/rules/attr-no-unnecessary-whitespace.js
@@ -11,7 +11,7 @@ export default {
 
       for (let i = 0; i < attrs.length; i++) {
         if (exceptions.indexOf(attrs[i].name) === -1) {
-          let match = /(\s*)=(\s*)/.exec(attrs[i].raw.trim())
+          const match = /(\s*)=(\s*)/.exec(attrs[i].raw.trim())
           if (match && (match[1].length !== 0 || match[2].length !== 0)) {
             reporter.error(
               "The attribute '" +
