da12822a13fcefdb1c7b827d70d19ecb9cf675cc
Christopher Quadflieg <chrissi92@hotmail.de>
2020-06-02 16:32:50 +0200

    feat: rebuild ruleset interface

--- src/core/types.ts
+++ src/core/types.ts
@@ -7,56 +7,61 @@ export interface Rule {
   init(parser: HTMLParser, reporter: Reporter, options: unknown): void
 }
 
-export interface Ruleset {
-  'alt-require'?: boolean
-  'attr-lowercase'?: boolean | Array<string | RegExp>
-  'attr-no-duplication'?: boolean
-  'attr-no-unnecessary-whitespace'?: boolean
-  'attr-sorted'?: boolean
-  'attr-unsafe-chars'?: boolean
-  'attr-value-double-quotes'?: boolean
-  'attr-value-not-empty'?: boolean
-  'attr-value-single-quotes'?: boolean
-  'attr-whitespace'?: boolean
-  'doctype-first'?: boolean
-  'doctype-html5'?: boolean
-  'head-script-disabled'?: boolean
-  'href-abs-or-rel'?: 'abs' | 'rel'
-  'id-class-ad-disabled'?: boolean
-  'id-class-value'?:
-    | 'underline'
-    | 'dash'
-    | 'hump'
-    | { regId: RegExp; message: string }
-  'id-unique'?: boolean
-  'inline-script-disabled'?: boolean
-  'inline-style-disabled'?: boolean
-  'input-requires-label'?: boolean
-  'script-disabled'?: boolean
-  'space-tab-mixed-disabled'?:
-    | boolean
-    | 'space'
-    | 'space1'
-    | 'space2'
-    | 'space3'
-    | 'space4'
-    | 'space5'
-    | 'space6'
-    | 'space7'
-    | 'space8'
-    | 'tab'
-  'spec-char-escape'?: boolean
-  'src-not-empty'?: boolean
-  'style-disabled'?: boolean
-  'tag-pair'?: boolean
-  'tag-self-close'?: boolean
-  'tagname-lowercase'?: boolean
-  'tagname-specialchars'?: boolean
-  'tags-check'?: { [tagName: string]: Record<string, unknown> }
-  'title-require'?: boolean
-  // There may be other unknown rules
-  [ruleId: string]: unknown
+export type RuleSeverity = 'off' | 'warn' | 'error'
+export type RuleConfig<Options = Record<string, unknown>> =
+  | RuleSeverity
+  | [RuleSeverity, Options | undefined]
+
+export interface BaseRuleset {
+  'alt-require'?: RuleConfig<never>
+  'attr-lowercase'?: RuleConfig<{ exceptions: Array<string | RegExp> }>
+  'attr-no-duplication'?: RuleConfig<never>
+  'attr-no-unnecessary-whitespace'?: RuleConfig<never>
+  'attr-sorted'?: RuleConfig<never>
+  'attr-unsafe-chars'?: RuleConfig<never>
+  'attr-value-double-quotes'?: RuleConfig<never>
+  'attr-value-not-empty'?: RuleConfig<never>
+  'attr-value-single-quotes'?: RuleConfig<never>
+  'attr-whitespace'?: RuleConfig<never>
+  'doctype-first'?: RuleConfig<never>
+  'doctype-html5'?: RuleConfig<never>
+  'head-script-disabled'?: RuleConfig<never>
+  'href-abs-or-rel'?: RuleConfig<{ mode: 'absolute' | 'relative' }>
+  'id-class-ad-disabled'?: RuleConfig<never>
+  'id-class-value'?: RuleConfig<{
+    mode: 'underline' | 'dash' | 'hump' | { regId: RegExp; message: string }
+  }>
+  'id-unique'?: RuleConfig<never>
+  'inline-script-disabled'?: RuleConfig<never>
+  'inline-style-disabled'?: RuleConfig<never>
+  'input-requires-label'?: RuleConfig<never>
+  'script-disabled'?: RuleConfig<never>
+  'space-tab-mixed-disabled'?: RuleConfig<
+    { mode: 'tab' } | { mode: 'space'; size?: number }
+  >
+  'spec-char-escape'?: RuleConfig<never>
+  'src-not-empty'?: RuleConfig<never>
+  'style-disabled'?: RuleConfig<never>
+  'tag-pair'?: RuleConfig<never>
+  'tag-self-close'?: RuleConfig<never>
+  'tagname-lowercase'?: RuleConfig<never>
+  'tagname-specialchars'?: RuleConfig<never>
+  'tags-check'?: RuleConfig<{
+    [tagName: string]: {
+      selfclosing?: boolean
+      redundantAttrs?: string[]
+      attrsRequired?: string[]
+      attrsOptional?: string[][]
+    }
+  }>
+  'title-require'?: RuleConfig<never>
 }
+
+export interface CustomRuleset {
+  [ruleId: string]: RuleConfig<unknown>
+}
+
+export type Ruleset = BaseRuleset & CustomRuleset
 
 export const enum ReportType {
   error = 'error',
