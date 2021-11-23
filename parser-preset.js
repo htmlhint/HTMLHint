ebcd6607365f2a6072ca528ebff7bd2d4d3b7fef
Nick Schonning <nschonni@gmail.com>
2021-11-23 12:42:30 -0500

    refactor: move eslint config to type overrides

--- parser-preset.js
+++ parser-preset.js
@@ -1,6 +1,6 @@
 module.exports = {
   parserOpts: {
-    headerPattern: /^(\w*)\((\w*)\)\:\s(.*)$/,
+    headerPattern: /^(\w*)\((\w*)\):\s(.*)$/,
     headerCorrespondence: ['type', 'scope', 'subject'],
   },
 }
