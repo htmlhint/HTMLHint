6ba83235f8c9c64926fde7c92d252f255e61d3b9
Nick Schonning <nschonni@gmail.com>
2021-11-23 20:20:29 -0500

    chore: update ESLint to v8

--- .eslintrc.js
+++ .eslintrc.js
@@ -40,6 +40,7 @@ module.exports = {
 
         '@typescript-eslint/adjacent-overload-signatures': 'off',
         '@typescript-eslint/ban-ts-comment': 'off',
+        '@typescript-eslint/no-unsafe-argument': 'off',
         '@typescript-eslint/no-unsafe-assignment': 'off',
         '@typescript-eslint/no-unsafe-call': 'off',
         '@typescript-eslint/no-unsafe-member-access': 'off',
