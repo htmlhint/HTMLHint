312f5e5c8cd2568ab6d3059aa5c002d148ee4400
Nick Schonning <nschonni@gmail.com>
2022-01-23 15:46:03 -0500

    test: look for missing documentation of rules

---
+++ test/rules/documentation.spec.js
@@ -0,0 +1,33 @@
+const expect = require('expect.js')
+const fs = require('fs')
+const path = require('path')
+
+describe('Rules documentation', () => {
+  let rules = fs
+    .readdirSync(path.join(__dirname, '..', '..', 'src', 'core', 'rules'))
+    .map((rule) => rule.replace('.ts', ''))
+
+  // remove the index.ts that includes all the rules as it is covered by list-rules.md
+  const index = rules.indexOf('index')
+  rules.splice(index, 1)
+
+  let docs = fs
+    .readdirSync(
+      path.join(__dirname, '..', '..', 'docs', 'user-guide', 'rules')
+    )
+    .map((doc) => doc.replace('.md', ''))
+
+  const rulesListPage = fs.readFileSync(
+    path.join(__dirname, '..', '..', 'docs', 'user-guide', 'list-rules.md'),
+    'utf-8'
+  )
+
+  rules.forEach((rule) => {
+    it(`${rule} should have a documentation page`, () => {
+      expect(docs).to.contain(rule)
+    })
+    it(`${rule} should be on the list-rules.md`, () => {
+      expect(rulesListPage).to.contain(rule)
+    })
+  })
+})
