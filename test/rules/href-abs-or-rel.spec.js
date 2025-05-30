771a5c3dd113e92fdf84920eb790dba2b040e870
Christian Oliff <christianoliff@pm.me>
2025-05-30 11:55:48 +0900

    cli: list rules alphabetically (#1599)
    * cli: list rules alphabetically
    
    * fix URL length

--- test/rules/href-abs-or-rel.spec.js
+++ test/rules/href-abs-or-rel.spec.js
@@ -20,7 +20,7 @@ describe(`Rules: ${ruleId}`, () => {
 
   it('Href value is absolute with abs mode should not result in an error', () => {
     const code =
-      '<a href="http://www.alibaba.com/">aaa</a><a href="https://www.alibaba.com/">bbb</a><a href="tel:12345678">ccc</a><a href="javascript:void()">ddd</a>'
+      '<a href="https://www.example.com/">aaa</a><a href="https://www.example.com/">bbb</a><a href="tel:12345678">ccc</a><a href="javascript:void()">ddd</a>'
     ruleOptions[ruleId] = 'abs'
     const messages = HTMLHint.verify(code, ruleOptions)
     expect(messages.length).toBe(0)
@@ -28,7 +28,7 @@ describe(`Rules: ${ruleId}`, () => {
 
   it('Href value is not relative with rel mode should result in an error', () => {
     const code =
-      '<a href="http://www.alibaba.com/">aaa</a><a href="https://www.alibaba.com/">bbb</a><a href="tel:12345678">ccc</a><a href="javascript:void()">ddd</a>'
+      '<a href="https://www.example.com/">aaa</a><a href="https://www.example.com/">bbb</a><a href="tel:12345678">ccc</a><a href="javascript:void()">ddd</a>'
     ruleOptions[ruleId] = 'rel'
     const messages = HTMLHint.verify(code, ruleOptions)
     expect(messages.length).toBe(2)
@@ -37,7 +37,7 @@ describe(`Rules: ${ruleId}`, () => {
     expect(messages[0].col).toBe(3)
     expect(messages[1].rule.id).toBe(ruleId)
     expect(messages[1].line).toBe(1)
-    expect(messages[1].col).toBe(44)
+    expect(messages[1].col).toBe(45)
   })
 
   it('Href value is relative with rel mode should not result in an error', () => {
